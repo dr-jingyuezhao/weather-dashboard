6973f6430efc5c2acdf4e04da73db8ab7b54d1e4
Jasmine <dr.jingyuezhao@outlook.com>
2023-01-29 14:00:15 +0000

    add a function on click to make the ajax request to the weather API with search input of city name and API key, and create a function to update page with JSON data on weather

---
+++ assets/js/draft.js
@@ -0,0 +1,16 @@
+    // add a bootstrap card in section #today
+    var timeblockEl = $(`<div id="hour${i + 9}" class="row time-block">
+    <div class="hour col-1">${timeblocks[i]}</div>
+    <textarea class="col-10 description"></textarea>
+    <button class="btn saveBtn col-1"><i class="fas fa-save"></i></button>
+    </div>`);
+    containerEl.append(timeblockEl);
+
+
+    var titleTd = $("<td>").text(response.Title);
+    <div class="card">
+        <h5 class="card-title">Card title</h5>
+        <img src="..." class="card-img-top" alt="...">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,77 @@
-alert("linked successfully");
+// Create a weather dashboard with form inputs.
+// API call: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+var apiKey = "4338bc6e6ee374a0322f73ff5e6b9efd";
+
+// * takes API data (JSON/object) and turns it into elements on the page
+// * @param {object} NYTData - object containing NYT API data
+// */
+
+
+// CLICK HANDLER
+// .on("click") function associated with the Search Button
+$("#search-button").on("click", function (event) {
+    // This line allows us to take advantage of the HTML "submit" property
+    // This way we can hit enter on the keyboard and it registers the search
+    // (in addition to clicks). Prevents the page from reloading on form submit.
+    event.preventDefault();
+
+    // Empty the region associated with the articles
+    $("#today").empty();
+    $("#forecast").empty();
+    // Build the query URL for the ajax request to the NYT API
+    var cityName = $("#search-input").val().trim();
+    console.log(cityName);
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+    console.log(queryURL);
+
+    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+    // The data then gets passed as an argument to the updatePage function
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(updatePage);
+});
+
+function updatePage(weatherData) {
+    console.log(weatherData);
+    // When a user searches for a city they are presented with current
+
+    // and future conditions for that city
+
+
+    // and that city is added to the search history.
+
+}
+
+
+
+
+
+
+
+// When a user views the current weather conditions for that city they are presented with:
+// The city name
+
+
+
+// The date
+
+// An icon representation of weather conditions
+
+// The temperature
+
+// The humidity
+
+// The wind speed
+
+// When a user views future weather conditions for that city they are presented with a 5-day forecast that displays:
+
+// The date
+
+// An icon representation of weather conditions
+
+// The temperature
+
+// The humidity
+
+// When a user clicks on a city in the search history they are again presented with current and future conditions for that city.
